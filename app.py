9bdb02641ed5a0195a6a7b6067e24750f54e0afa
Saatwik kumar Yadav <66209756+skypank-coder@users.noreply.github.com>
2026-04-06 19:13:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         "app:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=7860,
         reload=False
     )
